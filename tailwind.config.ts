38ee67145cb1e1fdf78c150a85294292bf9df4d4
Bora Özkoç <boraozkoc@hotmail.com>
2025-01-30 01:26:14 +0300

    TechSctack animation fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,12 +20,12 @@ const config: Config = {
       keyframes: {
         scroll: {
           to: {
-            transform: "translate(calc(-50% - 0.5rem))",
+            transform: "translate(calc(-50% - 1rem))",
           },
         },
         "infinite-scroll": {
           from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-50%)" },
+          to: { transform: "translateX(calc(-50% - 0.2rem))" },
         },
       },
     },
