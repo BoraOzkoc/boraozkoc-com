f2837889d157c78ccefe4e3c181971d010aa2ad9
Bora Özkoç <boraozkoc@hotmail.com>
2025-02-08 19:50:16 +0300

    Update Bio component layout with responsive design

--- src/app/Components/Bio.tsx
+++ src/app/Components/Bio.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 function Bio() {
 	return (
-		<div>
-			<span>
-				{`Hi, I'm Bora Özkoç, a Software Engineering graduate from University of Economics. With 3 years of game development experience using C# & Unity, I'm currently leveraging my technical background to expand my expertise into full-stack development. I work with React, JavaScript, TypeScript, Tailwind CSS, and Next.js to build modern web applications.`}
-			</span>
+		<div className="flex w-full flex-row">
+			<div className="w-1/2">
+				<span>
+					{`Hi, I'm Bora Özkoç, a Software Engineering graduate from University of Economics. With 3 years of game development experience using C# & Unity, I'm currently leveraging my technical background to expand my expertise into full-stack development. I work with React, JavaScript, TypeScript, Tailwind CSS, and Next.js to build modern web applications.`}
+				</span>
+			</div>
+			<div className="w-1/2 border"></div>
 		</div>
 	);
 }
